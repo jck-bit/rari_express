90cd2c21d725e5be0442b98c7f69c37503487d7d
jck-bit <jckkinyanjui@gmail.com>
2024-06-23 01:22:50 +0300

    added cors

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { Pool } = require('pg');
 const dotenv = require('dotenv').config()
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const port = 3000;
 
 const pool = new Pool({
