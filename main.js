4ecd653196c02b4e4ec538b7ff3099e2828fbcb5
jck-bit <jckkinyanjui@gmail.com>
2024-06-29 15:12:57 +0300

    updated the login

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const bcrypt = require('bcryptjs');
 const authMiddleware = require('./authMiddlware')
 const redisClient = require('./redisClient');
 
-
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -126,31 +125,26 @@ app.delete('/images/:id', authMiddleware, async (req, res) => {
   }
 });
 
-
-
 app.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    const userResult = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+    const user = userResult.rows[0];
 
-    if (result.rows.length === 0) {
-      return res.status(400).json({ error: 'Invalid email or password' });
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid email or password' });
     }
 
-    const user = result.rows[0];
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ error: 'Invalid email or password' });
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (!validPassword) {
+      return res.status(401).json({ error: 'Invalid email or password' });
     }
 
-    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, {
-      expiresIn: '3m',
-    });
-
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
     res.json({ token });
   } catch (err) {
-    console.error('Error querying the database:', err);
+    console.error('Error logging in:', err);
     res.status(500).json({ error: 'Server error' });
   }
 });
