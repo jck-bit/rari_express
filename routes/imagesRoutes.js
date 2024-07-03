cc6424af619fa5e34026254b6e3a923722cbe50a
jck-bit <jckkinyanjui@gmail.com>
2024-07-04 00:22:25 +0300

    removed unnecesary files
    added authmiddleware to get-images

--- routes/imagesRoutes.js
+++ routes/imagesRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { getImages, getImageById,  deleteImage, getSavedImages, PostSaveImage } = require('../controllers/imagesController');
 const authMiddleware = require('../middleware/authMiddlware');
 
-router.get('/images', getImages);
-router.get('/images/:id', getImageById);
+router.get('/images',authMiddleware, getImages);
+router.get('/images/:id',authMiddleware, getImageById);
 router.get('/saved-images', authMiddleware, getSavedImages);
 router.post('/saved-images', authMiddleware, PostSaveImage);
 router.delete('/images/:id', authMiddleware, deleteImage);
